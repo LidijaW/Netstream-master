1fb30570a8fb89d0de271ddb32f7adcb83efae4c
lsebalj1 <lusebalj@student.unipu.hr>
2024-01-30 16:01:57 +0100

    Razina i Korisnik model

---
+++ Properties/Model/Korisnik.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Netstream.Properties.Model
+{
+    public class Korisnik
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Ime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Prezime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Korisnicko_ime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Lozinka { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        public bool Aktivan { get; set; }
+
+        public int Razina_ID { get; set; }
+
+        [ForeignKey("Razina_ID")]
+        public virtual Razina Razina { get; set; }
+    }
+
+}
---
+++ Properties/Model/Razina.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Netstream.Properties.Model
+{
+    
+    public class Razina
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Naziv { get; set; }
+
+        public virtual ICollection<Korisnik> Korisnici { get; set; }
+    }
+
+}
